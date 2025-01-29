b24f7c2b88e0e7ed8740085b05a982e2130a367b
Marcus Chaves <85702234+vinniboy021@users.noreply.github.com>
2025-01-28 21:04:36 -0300

    Update attacking-machine.sh

--- attacking-machine.sh
+++ attacking-machine.sh
@@ -19,7 +19,7 @@ apt-get install -y kali-linux-large
 echo ""
 #2. Instalar dependências essenciais
 echo "Instalando dependências essenciais..."
-apt-get install -y python3 python2.7 golang apache2 selinux-basics hcxtools nmap kolourpaint whois ccrypt hcxdumptools cifs-utils pacu routersploit netcat reaver net-tools hping3 tcpdump netcat-traditional mdk4 git freeradius wget maltego keepassxc-full otpclient duplicity torbrowser-launcher
+apt-get install -y python3 python2.7 golang apache2 selinux-basics hcxtools nmap kolourpaint whois ccrypt cifs-utils pacu routersploit reaver net-tools hping3 tcpdump netcat-traditional mdk4 git freeradius wget maltego keepassxc-full otpclient duplicity torbrowser-launcher
 echo ""
 #3. Instalar o Nuclei
 echo "Instalando o Nuclei..."
@@ -32,27 +32,19 @@ apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 echo ""
-#5. Instalar o Scout Suite 
-echo "Instalando o Scout Suite ..."
-pip install scoutsuite    
-echo ""
-#6. Instalar o Paramiko
-echo "Instalando o Paramiko ..."
-pip install paramiko    
-echo ""
-#7. Instalar o WPScan
+#5. Instalar o WPScan
 echo "Instalando o WPScan ..."
 gem install wpscan 
 echo ""
-#8. Atualizar pacotes do sistema
+#6. Atualizar pacotes do sistema
 echo "Atualizando pacotes do sistema..."
 apt-get update && apt-get upgrade -y
 echo ""
-#9. Instalar o Kali Win-KeX (última etapa)
+#7. Instalar o Kali Win-KeX (última etapa)
 echo "Instalando o Kali Win-KeX..."
 apt-get install -y kali-win-kex
 echo ""
-#10. Configurar o Kex
+#8. Configurar o Kex
 echo "Configurando o Win-KeX..."
 kex --esm
 
