ebff808b3a92f0917ea5a776c5019e54312711ba
Marcus Chaves <85702234+vinniboy021@users.noreply.github.com>
2025-02-25 20:42:31 -0300

    Update attacking-machine.sh

--- attacking-machine.sh
+++ attacking-machine.sh
@@ -19,7 +19,7 @@ apt-get install -y kali-linux-large
 echo ""
 #2. Instalar dependências essenciais
 echo "Instalando dependências essenciais..."
-apt-get install -y python3 python2.7 golang apache2 selinux-basics hcxtools nmap kolourpaint whois ccrypt cifs-utils pacu routersploit reaver net-tools hping3 tcpdump netcat-traditional mdk4 git freeradius wget maltego keepassxc-full otpclient duplicity torbrowser-launcher netcat-openbsd socat dmitry whatweb python3-venv wget2
+apt-get install -y python3 python2.7 libssl-dev pkg-config golang apache2 selinux-basics hcxtools nmap kolourpaint whois ccrypt cifs-utils pacu routersploit reaver net-tools hping3 tcpdump netcat-traditional mdk4 git freeradius wget maltego keepassxc-full otpclient duplicity torbrowser-launcher netcat-openbsd socat dmitry whatweb python3-venv wget2 cargo 
 echo ""
 #3. Instalar o Nuclei
 echo "Instalando o Nuclei..."
@@ -40,13 +40,23 @@ echo ""
 echo "Atualizando pacotes do sistema..."
 apt-get update && apt-get upgrade -y
 echo ""
-#7. Instalar o Kali Win-KeX (última etapa)
+#7. Instalar o Kali Win-KeX
 echo "Instalando o Kali Win-KeX..."
 apt-get install -y kali-win-kex
 echo ""
 #8. Configurar o Kex
 echo "Configurando o Win-KeX..."
 kex --esm
+echo ""
+#9. Instalar Cracken
+echo "Instalando Cracken..."
+git clone https://github.com/shmuelamar/cracken
+cd cracken
+cargo build --release
+cd target
+cd release
+cp cracken /usr/bin
+echo ""
 
 # Finalização
 echo "Configuração concluída! O ambiente está pronto."
